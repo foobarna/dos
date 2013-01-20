fc89fc82f7e3d0b9aaff1c42ab8ccfd3a302c6a6
icanblink <b.bogdu@gmail.com>
2013-01-20 13:49:50 +0200

    update

---
+++ threads/list.h
@@ -0,0 +1,79 @@
+typedef struct elem {
+	int value;
+	int worker;
+	struct elem* next;
+}elem;
+
+typedef struct {
+	elem* head;
+	elem* last;
+	int size;
+}list;
+
+void add(list* l, int value, int worker) {
+	if (!l->head) {
+		l->head = malloc(sizeof(elem));
+		l->head->value = value;
+		l->head->worker = worker;
+		l->head->next = NULL;
+		l->last = l->head;
+		l->size = 1;
+	} else {
+		elem* e = malloc(sizeof(elem));
+		e->value = value;
+		e->worker = worker;
+		e->next = NULL;
+		l->last->next = e;
+		l->last = e;
+		l->size++;
+	}
+}
+
+int delete_value(list* l, int value) {
+	elem* p = l->head, *prev = NULL;
+	while (p!=NULL) {
+		if (p->value <= value) {
+				if(p==l->head){
+				l->head=p->next;
+				free(p);
+				l->size=l->size-1;
+				return 1;
+			} else {
+				prev->next=p->next;
+				free(p);
+				l->size=l->size-1;
+				return 1;
+			}
+		} else {
+			prev=p;
+			p=p->next;
+		}
+	}
+	return 0;
+}
+
+void delete_all(list* l, int value) {
+	while(delete_value(l,value)) {}
+}
+
+void generate_all(list* l, int size, int workers) {
+	int i,value,worker;
+	for (i = 0; i < size; i++) {
+		srand(i);
+		value = rand();
+		worker = i % workers;
+		add(l,value,worker);
+	}
+}
+
+void print(list* l) {
+	if (l->head == NULL) {
+		printf("NULL list!\n");
+		return;
+	}
+	elem* e = l->head;
+	while(e) {
+		printf("(value=%d, worker=%d )\n",e->value,e->worker );
+		e = e->next;
+	}
+}
